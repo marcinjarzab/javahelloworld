ff5976b25fa073d66a5e2999072ac1054d50c5bf
MJ <mj@agh.edu.pl>
2016-02-26 10:58:01 +0000

    new modification

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello from java ...");
+		System.out.println("hello from java again ...");
 		System.exit(0);
 	}
 }
